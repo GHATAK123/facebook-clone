d597005d4970dc69a868398546b501ce72f0cdb3
Prakash Anand <ppa58514@gmail.com>
2020-08-28 01:19:35 +0530

    Navigation bar

---
+++ src/Header.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import {Avatar,IconButton} from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Facebook_colored_svg_copy-512.png" alt=""/>
+        <div className="header__input">
+        <SearchIcon />
+         <input type="text" />
+        </div>
+       
+
+      
+      
+      </div>
+      <div className="header__middle">
+        <div className="header__option">
+           <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+           <FlagIcon fontSize="large" />
+        </div>
+        
+        <div className="header__option">
+           <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        
+          
+        <div className="header__option">
+           <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+
+        <div className="header__option">
+           <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      
+      
+    </div>
+    <div className="header__right">
+      <div className="header__info">
+        <Avatar/>
+        <h4>Prakash</h4>
+      </div>
+      <IconButton>
+        <AddIcon />
+      </IconButton>
+      <IconButton>
+        <ForumIcon />
+      </IconButton>
+      <IconButton>
+        <NotificationsActiveIcon />
+      </IconButton>
+      <IconButton>
+        <ExpandMoreIcon />
+      </IconButton>
+      
+      
+    </div>
+
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCI8Ru6dzcN8Tyb6d0u0aljPRyheH35M3Q",
+  authDomain: "facebook-clone-a51ed.firebaseapp.com",
+  databaseURL: "https://facebook-clone-a51ed.firebaseio.com",
+  projectId: "facebook-clone-a51ed",
+  storageBucket: "facebook-clone-a51ed.appspot.com",
+  messagingSenderId: "319712693077",
+  appId: "1:319712693077:web:8bfeec9e437f884bd67ddc",
+  measurementId: "G-JF5VSBRH1M"
+};
